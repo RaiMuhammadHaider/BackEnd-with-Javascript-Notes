bf668a079b52849d1504fc248028d0de6f42d397
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-10-11 04:46:25 -0700

    fix dot env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+
 dotenv.config({
-    path : './env' // config dotenv
+    path : './.env' // config dotenv
 })
-connectDB() // connect MongoDB
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 9000 , ()=>{
+        console.log(`Port is listening at ${process.env.PORT}`);
+        
+    })
+    app.on('error' , (error)=>{
+        console.log(`Error Found ! : ${error} `);
+        
+    })
+    
+}).catch((error)=>{
+    console.log(`MongoDB Connection Failed !!  ${error}`);
+    
+}) // connect MongoDB
