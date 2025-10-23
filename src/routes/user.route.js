3c852c7e132e59a423c92620e8f3efae1544b6a0
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-10-23 01:55:52 -0700

    middleware for avatar and coverImage

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,19 @@
-import { userController } from "../controllers/user.controller.js";
+import { userRegisterController } from "../controllers/user.controller.js";
 import { Router } from "express";
+import {upload} from '../middlewares/multer.middleware.js'
 
 const router = Router()
-router.route('/register').post(userController) // router for user registration  
+router.route('/register').post(
+    upload.fields([ // multer middleware for handling multipart/form-data for file upload 
+        {
+            name : "avatar", // field name in the form
+            maxCount : 1,
+        },{
+            name : "coverImage",
+            maxCount : 1,
+        }
+    ])
+
+    , userRegisterController) // router for user registration
 
 export default router
