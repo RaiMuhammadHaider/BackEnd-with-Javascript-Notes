d62892f5c76bd17d3bf3dcb8482f75b4d5967541
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2026-06-15 20:22:02 +0500

    flow to upload file though multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,9 +5,14 @@ const storage = multer.diskStorage({
     cb(null, './public/temp') // specify the destination directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname) 
+    cb(null, file.originalname)
   }
 })
 
-export const upload = multer({  storage })
-// this middleware saves the file to local temp folder before uploading to cloudinary 
+export const upload = multer({  storage }) // file ka size kiya ho ga file ki type kiya ho gi vo sab multer ma hi define kiya jata
+// this middleware saves the file to local temp folder before uploading to cloudinary
+/*
+process kuch is terha sy jota ha k sab sy phaly user file upload krta ha
+user upload file ==> multern ==> save the file into local storage or punlic/temp ==> upload on cloudinary ==> get cloudinary url ==> save Url in db ==> Delete temp folder into local storage ot public/temp
+
+*/
