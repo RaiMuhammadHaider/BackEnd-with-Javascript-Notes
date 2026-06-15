59f866a1d38a606228c069af96d0d4023ce9a44b
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2026-06-15 20:36:39 +0500

    fix Errors in User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt'
 const userSchema = new Schema({
 username : {
     type : String,
-    require : true,
+    required : true,
     unique : true,
     lowercase : true,
     trim : true,
@@ -13,7 +13,7 @@ username : {
 },
 email : {
     type : String,
-    require : true,
+    required : true,
     unique : true,
     lowercase : true,
     trim : true,
@@ -22,7 +22,7 @@ email : {
 
 fullName : {
     type : String,
-    require : true,
+    required : true,
     trim : true,
     index : true,
 
@@ -41,24 +41,26 @@ watchHistory : [
         type : Schema.Types.ObjectId,
         ref : "Video"
     }
-], 
+],
 password : {
     required : [true , "password is required"],
     type : String
 },
 refreshToken : {
-    type : String 
+    type : String
 }}, {timestamps : true})
-userSchema.pre("save"  , async function (next) {
+userSchema.pre("save"  , async function (next) { //this one is a middleware it says before save kuch kr do . pre mongoose ki terf sy ak middleware ha jo k parameters ma ak action klata ha yani save ka action hm yahan use kr ry hn  us k bad ak function leta ha yani us method pe  kon sa kam ho vo functionlity leta ha
     if (!this.isModified("password")) return next()
-        this.password = bcrypt.hash(this.password , 10)
-    next() 
+        this.password = await bcrypt.hash(this.password , 10)
+    next()
 } )
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password , this.password)
 
 }
-userSchema.methods.generateAccessToken = function () {
+
+// both are jwt token but both use are different
+userSchema.methods.generateAccessToken = function () { // these are our custome properties to make changes in schema or sb
     return jwt.sign({
         _id : this._id,
         email : this.email,
@@ -67,15 +69,16 @@ userSchema.methods.generateAccessToken = function () {
     } ,process.env.ACCESS_TOKEN_SECRET , {
         expiresIn : process.env.ACCESS_TOKEN_EXPIRY
     }  )
-    
+   
 }
 userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign({
+    return jwt.sign({ // jwt is a bearar token mean jo is ko bear krta ha usi ko ya sahi man leta ha , yani token jis k bi pass ha ma us ko data dy don ga mean , ya k key ki terha ha ak chabi ki terha jis k pass bi vo key chabi ho gi jwt us ko data dy gy ga
         _id : this._id,
-    }, 
+    },
 process.env.REFRESH_TOKEN_SECRET , {
         expiresIn : process.env.REFRESH_TOKEN_EXPIRY
     })
-    
+   
 }
 export const User = mongoose.model("User" , userSchema )
+// we store refresh_token in db but ve did not save or store the access_token in db
